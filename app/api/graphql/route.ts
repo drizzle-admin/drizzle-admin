d4875e7050796f9499758c25e9049fe1440caccb
Travis Luong <travis@travisluong.com>
2025-01-08 20:01:58 -0800

    custom graphql object for querying tables

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -3,7 +3,38 @@ import { db } from "@/lib/db";
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 
-const { schema } = buildSchema(db);
+import {
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from "graphql";
+
+// const { schema } = buildSchema(db);
+const { entities } = buildSchema(db);
+
+const TablesType = new GraphQLObjectType({
+  name: "Tables",
+  description: "all the tables",
+  fields: {
+    name: { type: GraphQLString },
+  },
+});
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "Query",
+    fields: {
+      usersTable: entities.queries.usersTable,
+      tables: {
+        resolve: async () => {
+          return [{ name: "foo" }];
+        },
+        type: new GraphQLList(TablesType),
+      },
+    },
+  }),
+});
 
 const server = new ApolloServer({ schema });
 
