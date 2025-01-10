ba361a78124472249f8cbd181cdd1445a22ef14e
Travis Luong <travis@travisluong.com>
2025-01-10 14:32:28 -0800

    add columns to graphql response

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -2,6 +2,8 @@ import { buildSchema } from "drizzle-graphql";
 import { db } from "@/lib/db";
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import * as dbSchema from "@/lib/schema";
+import { getTableConfig } from "drizzle-orm/pg-core";
 
 import {
   GraphQLList,
@@ -18,6 +20,7 @@ const TablesType = new GraphQLObjectType({
   description: "all the tables",
   fields: {
     name: { type: GraphQLString },
+    columns: { type: new GraphQLList(GraphQLString) },
   },
 });
 
@@ -28,7 +31,17 @@ const schema = new GraphQLSchema({
       usersTable: entities.queries.usersTable,
       tables: {
         resolve: async () => {
-          return [{ name: "foo" }];
+          const tables = Object.keys(entities.queries)
+            .filter((table) => !table.endsWith("Single"))
+            .map((table) => {
+              const tableConfig = getTableConfig(dbSchema.usersTable);
+              const columns = tableConfig.columns.map((col) => col.name);
+              return {
+                name: table,
+                columns: columns,
+              };
+            });
+          return tables;
         },
         type: new GraphQLList(TablesType),
       },
