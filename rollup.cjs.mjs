6f54a6ac9bcc835797e8345b39cdb34defea6d0a
Travis Luong <travis@travisluong.com>
2025-01-11 11:56:39 -0800

    add peer deps external plugin and dts

--- rollup.cjs.mjs
+++ rollup.cjs.mjs
@@ -1,9 +1,12 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import PeerDepsExternalPlugin from "rollup-plugin-peer-deps-external";
+import dts from "rollup-plugin-dts";
 
 import packageJson from "./package.json" assert { type: "json" };
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [
   {
     input: "components/index.ts",
@@ -15,11 +18,17 @@ export default [
       },
     ],
     plugins: [
+      PeerDepsExternalPlugin(),
       resolve(),
       commonjs(),
       typescript({
         tsconfig: "./tsconfig.cjs.json",
       }),
     ],
+  },
+  {
+    input: "dist/cjs/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "cjs" }],
+    plugins: [dts()],
   },
 ];
--- rollup.esm.mjs
+++ rollup.esm.mjs
@@ -3,7 +3,10 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 
 import packageJson from "./package.json" assert { type: "json" };
+import PeerDepsExternalPlugin from "rollup-plugin-peer-deps-external";
+import dts from "rollup-plugin-dts";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [
   {
     input: "components/index.ts",
@@ -15,11 +18,17 @@ export default [
       },
     ],
     plugins: [
+      PeerDepsExternalPlugin(),
       resolve(),
       commonjs(),
       typescript({
         tsconfig: "./tsconfig.esm.json",
       }),
     ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
   },
 ];
